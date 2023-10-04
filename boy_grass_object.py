3c480bfba881684e750c5a431721b6f65fcac31c
roal518 <roalqwerty@gmail.com>
2023-10-04 20:01:10 +0900

    10/04 강의 진도
    추가적으로 Ball 객체 생성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,7 +1,6 @@
 from pico2d import *
-
+import random
 # Game object class here
-
 def handle_events():
     global running
     events = get_events()
@@ -10,13 +9,58 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-
+def update_world():
+    for o in world:
+        o.update()
+    pass
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+def reset_world():
+    global running
+    global grass
+    global team
+    global world
+#월드 하나로 모든 요소 통제 가독성이 good
+#그래서 cpp 하라는것?
+    running =True
+    world=[]
+    grass=Grass()
+    world.append(grass)
+    team =[Boy() for i in range(11)]
+    world +=team
+class Boy:
+    def __init__(self):
+        self.x,self.y=random.randint(0,300),90
+        self.frame =0
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.frame = (self.frame+1)%8
+        self.x +=random.randint(0,4)
+    def draw(self):
+        self.image.clip_draw(self.frame*100,0,100,100,self.x,self.y)
+class Ball:
+    def __init__(self):
+        self.x,self.y = random.randint(0,800),random.randint(499,500)
+        self.frame = 0
+        self.image = load_image('')
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+    def draw(self):
+        self.image.draw(400,30)
+    def update(self):
+        pass
 open_canvas()
-
+reset_world()
 # initialization code
-
 # game main loop code
-
+while running:
+    handle_events()
+    update_world()#game logic
+    render_world()#draw game world
+    delay(0.05)
 # finalization code
-
 close_canvas()
